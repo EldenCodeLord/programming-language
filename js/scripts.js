6cff5bdc82f5ab315099e69166c1c78e82a49ec4
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-03 23:50:49 -0700

    WIP: commit for the night.

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,20 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    e.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const question = $("input#question").val();
-    const otherQuestion = $("input#otherQuestion").val();
+  $("#formOne").submit((event) => {
+      event.preventDefault();
+      const person1Input = $("input#person1").val();
+      const person2Input = $("input#person2").val();
+      const animalInput = $("input#animal").val();
+      const question = $("input#question").val();
+      const otherQuestion = $("input#otherQuestion").val();
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".question").append(questionInput);
-    $(".otherQuestion").append(otherQuestionInput);
+      $(".person1").append(person1Input);
+      $(".person2").append(person2Input);
+      $(".animal").append(animalInput);
+      $(".question").append(questionInput);
+      $(".otherQuestion").append(otherQuestionInput);
 
-    $("#story").show();
-  });
+      $(".story").show();
+    });
 });
