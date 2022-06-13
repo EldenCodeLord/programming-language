614f781a83caec905d7c301bd945c4bbf770019d
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-12 23:12:48 -0700

    WIP:Working on JS bug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function() {  
    $("p").click(function() {
    $(".html-showing").toggle();
    $(".html-hidden").toggle();
@@ -9,19 +9,15 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("form#list").submit(function(event) {
       event.preventDefault();
-      const favoriteNumber = $("input#favoriteNumber").val();
-        if (favoriteNumber >= 10) {
+      const favoriteNumber = $("input#favNum").val();
+        if (favoriteNumber <= 10) {
           console.log('Try HTML!'); 
-        else if (favoriteNumber >= 100) 
+        } else if (favoriteNumber <= 100) {
           console.log('TRY CSS');
-        else (favoriteNumber >= 200) 
+        } else (favoriteNumber <= 200) {
           console.log('TRY JAVASCRIPT');
         }
 
-      $(".favoriteNumber").append(favoriteNumber);
-     
-
-      $("#list").show();
     });
 });
 
